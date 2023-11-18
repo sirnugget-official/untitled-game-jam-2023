8760b4d38092206be5a99bcb7d7e563f5c50ddb1
sirnugget_official <mayson.reader2023@gmail.com>
2023-11-18 16:31:01 +1100

    Auto stash before merge of "master" and "origin/master"e

---
+++ Area2D.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+signal hit
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
--- Player.gd
+++ Player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
+class_name PlayerCharacter
+
 @export var speed = 400
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
